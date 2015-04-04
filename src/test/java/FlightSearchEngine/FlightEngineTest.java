a7e951f44f28fa3236991fb79d3278a533bdd7a7
Alexander Elis <alex2789@gmail.com>
2015-04-04 22:27:48 +0000

    Implemented layover and return flight results

--- src/test/java/FlightSearchEngine/FlightEngineTest.java
+++ src/test/java/FlightSearchEngine/FlightEngineTest.java
@@ -109,6 +109,63 @@ public class FlightEngineTest {
                 "        30000," +
                 "        30);";
         stmt.executeUpdate(sql);
+        sql = "INSERT INTO Flights (" +
+                "        flightNumber," +
+                "        airline," +
+                "        departureTime," +
+                "        arrivalTime," +
+                "        departureLocation," +
+                "        arrivalLocation," +
+                "        price," +
+                "        seatsAvailable) " +
+                "VALUES (" +
+                "        255, " +
+                "        'KF', " +
+                "        '2015-03-30 09:10:00', " +
+                "        '2015-03-30 12:55:00', " +
+                "        'Copenhagen'," +
+                "        'Reykjavik'," +
+                "        30000," +
+                "        15);";
+        stmt.executeUpdate(sql);
+        sql = "INSERT INTO Flights (" +
+                "        flightNumber," +
+                "        airline," +
+                "        departureTime," +
+                "        arrivalTime," +
+                "        departureLocation," +
+                "        arrivalLocation," +
+                "        price," +
+                "        seatsAvailable) " +
+                "VALUES (" +
+                "        301, " +
+                "        'ER', " +
+                "        '2015-03-30 14:10:00', " +
+                "        '2015-03-30 15:55:00', " +
+                "        'Reykjavik'," +
+                "        'Akureyri'," +
+                "        30000," +
+                "        15);";
+        stmt.executeUpdate(sql);
+        sql = "INSERT INTO Flights (" +
+                "        flightNumber," +
+                "        airline," +
+                "        departureTime," +
+                "        arrivalTime," +
+                "        departureLocation," +
+                "        arrivalLocation," +
+                "        price," +
+                "        seatsAvailable) " +
+                "VALUES (" +
+                "        302, " +
+                "        'ER', " +
+                "        '2015-03-30 12:10:00', " +
+                "        '2015-03-30 15:55:00', " +
+                "        'Reykjavik'," +
+                "        'Akureyri'," +
+                "        30000," +
+                "        15);";
+        stmt.executeUpdate(sql);
         stmt.close();
         flightEngine = new FlightEngine(mockDBConn);
     }
@@ -159,7 +216,7 @@ public class FlightEngineTest {
         Flight departureFlight = departureFlights.get(0);
 
         assertNotNull(departureFlight.getDepartureTime());
-        assertEquals("2015-03-30 09:10:00",departureFlight.getDepartureTime());
+        assertEquals("2015-03-30T09:10",departureFlight.getDepartureTime().toString());
     }
 
     @Test
@@ -172,7 +229,7 @@ public class FlightEngineTest {
         Flight departureFlight = departureFlights.get(0);
 
         assertNotNull(departureFlight.getArrivalTime());
-        assertEquals("2015-03-30 12:55:00",departureFlight.getArrivalTime());
+        assertEquals("2015-03-30T12:55",departureFlight.getArrivalTime().toString());
     }
 
     @Test
@@ -228,10 +285,25 @@ public class FlightEngineTest {
     }
     @Test
     public void testGetResults() throws Exception {
-        Statement stmt = conn.createStatement();
         LocalDate departureDate =  LocalDate.of(2015,3,30);
         FlightQuery query = new FlightQuery(departureDate,"Reykjavik","London");
         List<FlightTrip> results = flightEngine.getResults(query);
         assertEquals(2,results.size());
+    }
+
+    @Test
+    public void testLayoverOneWay() {
+        LocalDate departureDate =  LocalDate.of(2015,3,30);
+        FlightQuery query = new FlightQuery(departureDate,"Copenhagen","Akureyri");
+        List<FlightTrip> results = flightEngine.getResults(query);
+        assertNotNull(results);
+        for (FlightTrip trip : results) {
+            assertEquals(2, trip.getDepartureFlights().size());
+        }
+    }
+
+    @Test
+    public void testTwoWayFlight() {
+        // TODO: Implement after database has been updated with flights from Icelandic locations to Reykjavik and from Reykjavik to foreign countries
     }
 }
